f4b0b0f44c3991592bde2e7ce1440928e7e18302
Spencer <spencer.bartonfisher@me.com>
2018-04-07 20:58:07 +0100

    removed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail'
-const API_KEY = 'AIzaSyB3rGcwa2hiopF7CjGVDMC_rZFEIJPABBc';
+const API_KEY = '';
 
 // Create a new component, this component should produce some HTML
 class App extends Component {
